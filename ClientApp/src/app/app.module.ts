9c7bba355cf3c41bb60e0282b67c324d63f40240
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-01-22 00:37:45 +0100

    add screen service

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './views/nav-menu/nav-menu.component';
@@ -35,7 +36,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       { path: 'settings', component: SettingsViewComponent, pathMatch: 'full' },
     ]),
     BrowserAnimationsModule,
-    NgxChartsModule
+    NgxChartsModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
